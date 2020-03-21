d18f89b24496a597b61f4ca0d66c3219c663ba72
Jari Moellenbernd <jari.moellenbernd@assecosol.com>
2020-03-21 11:01:32 +0100

    chore: add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }), EffectsModule.forRoot([AppEffects])],
   providers: [
     StatusBar,
     SplashScreen,
